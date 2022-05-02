6e0cb96456fc53f724e8f4f8b998ef1cca384fa5
Athirson Silva <athirson07@gmail.com>
2022-05-02 14:26:21 -0300

    Commit pré-favoritos

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, {useEffect, useState} from 'react';
 import { getPokemonData, getPokemons } from './api';
 import './App.css';
-import NavBar from './components/navbar';
-import Pokedex from './components/pokedex';
-import SearchBar from './components/searchbar'; 
+import NavBar from './components/Navbar';
+import Pokedex from './components/Pokedex';
+import SearchBar from './components/Searchbar'; 
 
 
 
 
 
 function App() {
+  const [page, setPage] = useState(0)
+  const [totalPages, setTotalPages] = useState(0)
   const [loading, setLoading] = useState(false)
   const [pokemons, setPokemons] = useState([])
+
+  const itensPerPage = 50;
+
   const fetchPokemons = async () => {
     try {
       setLoading(true)
-      const data = await getPokemons();
+      const data = await getPokemons(itensPerPage, itensPerPage * page);
       const promises = data.results.map(async (pokemon) => {
       //Lista de promessas
       return await getPokemonData(pokemon.url) 
@@ -25,22 +30,22 @@ function App() {
       const results = await Promise.all(promises)
       setPokemons(results);
       setLoading(false);
+      setTotalPages(Math.ceil(data.count / itensPerPage))
     } catch (error) {
       console.log("Fetch pokemon error:", error)
     }
-
   }
 
   useEffect(() => {
     console.log("Carregou!")
     fetchPokemons()
-  }, [])
+  }, [page])
   
 
   /*useEffectSnippet
   first - Quando carregar executa a função X
   return second - Quando o componente é destruído, executa a função Y
-  third - Quando o componente third for alterado, mesmo que no dom, o React é obrigado a renderizá-lo também, como se aplicado no virtual DOM 
+  third - Quando o componente third for alterado, mesmo que no dom, o React é obrigado a renderizá-lo novamente também, como se aplicado no virtual DOM 
   const fetchPokemons = async ()=> {
   useEffect(() => { 
     first
@@ -62,7 +67,12 @@ function App() {
         <SearchBar/>
       </div>
       <div>
-        <Pokedex pokemons={pokemons} loading={loading} /> 
+        <Pokedex 
+          pokemons={pokemons} 
+          loading={loading} 
+          page={page}
+          setPage={setPage} 
+          totalPages={totalPages}/> 
       </div> 
       
     </div>
--- src/components/pokedex.js
+++ src/components/pokedex.js
@@ -1,14 +1,29 @@
 import React from "react";
-import Pokemon from "./pokemon";
+import Pagination from "./Pagination";
+import Pokemon from "./Pokemon";
 
 const Pokedex = (props) => {
-    const{pokemons, loading} = props; //Parametros passados pelo app
-
+    const{pokemons, loading, page, setPage, totalPages} = props; //Parametros passados pelo app
+    const onPreviousClickHandler = () => {
+        if(page > 0) {
+            setPage(page-1)
+        }
+    }
+    const onNextClickHandler = () => {
+        if(page+1 < totalPages) {
+            setPage(page+1)
+        }
+    }
     return (
         <div>
             <div className="pokedex-header">
             <h1>Pokedex</h1>
-            <div>Paginação</div>
+            <Pagination
+                page={page+1}
+                totalPages={totalPages}
+                onPreviousClick={onPreviousClickHandler}
+                onNextClick={onNextClickHandler}
+            />
             </div>
             
             {loading ? (<div>Carregando, aguenta coração...</div>)
@@ -16,7 +31,10 @@ const Pokedex = (props) => {
                     {pokemons && pokemons.map((pokemon, index) => { //map funciona como um foreach, irá oassar por todos objetos da lista e chamará a função
                         //Em listas, cada filho deve ter uma key única, por isso o key={}
                         return (
-                            <Pokemon key={index} pokemon={pokemon}/>
+                            <Pokemon 
+                                key={index} 
+                                pokemon={pokemon}
+                            />
                         )
                     })}
                 </div>)}
